d91b23e808ffd7706a1b4588a66929bc18119d81
Yaroslav Pylypenko <sitegarant@gmail.com>
2019-10-29 23:09:59 +0100

    react component

---
+++ src/Car/Car.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+//----------------Primo stile------------------//
+// function car() {
+//     return (
+//         <h2>This is car component</h2>
+//     )
+// }
+
+
+//-----------------Secondo stile-----------------//
+// const car = () => {
+//     return (
+//         <div> This is car component </div>
+//     )
+// }
+
+//export default car
+
+export default () => (
+    <div> 
+        This is car component
+    </div>
+)
